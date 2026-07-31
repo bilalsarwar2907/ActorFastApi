060149fbdf5d2bb8542c3d67827ae4b9ba0b2ae9
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-08-01 00:00:27 +0200

    error removed

--- tests/test_actors.py
+++ tests/test_actors.py
@@ -4,7 +4,7 @@ from main import app
 client = TestClient(app)
 
 def test_create_actor():
-    response = client.post("/actors/", json={"id": 99, "name": "Test Actor", "age": 30})
+    response = client.post("/actors/", json={"id": 999, "name": "Test Actor", "age": 30, "gender": "Male"})
     assert response.status_code == 200
 
 def test_get_all_actors():
@@ -12,9 +12,9 @@ def test_get_all_actors():
     assert response.status_code == 200
 
 def test_get_actor_by_id():
-    response = client.get("/actors/99")
+    response = client.get("/actors/999")
     assert response.status_code == 200
 
 def test_delete_actor():
-    response = client.delete("/actors/99")
+    response = client.delete("/actors/999")
     assert response.status_code == 200
