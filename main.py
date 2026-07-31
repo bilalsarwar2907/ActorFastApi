df83264c60f5f7fd4c20a1dd5d2bd45c89cb359c
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-07-31 23:32:27 +0200

    Main.py is updated

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from routers import actor_router
+from database.database import create_tables
 
 app = FastAPI()
+create_tables()
 app.include_router(actor_router.router)
