957d2c739bab9ee96a8e9cf23a52c8d5ed0b9fdb
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-08-01 14:37:19 +0200

    import auth_router and add import models.user_entity

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from routers import actor_router
+from routers import actor_router, auth_router
 from database.database import create_tables
-import models.actor_entity  # registers the table with Base
+import models.actor_entity
+import models.user_entity  # registers users table with Base
 
 app = FastAPI()
 create_tables()
+app.include_router(auth_router.router)
 app.include_router(actor_router.router)
